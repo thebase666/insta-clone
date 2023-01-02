945ab1e6ec3c8e8abaa5ea1b22cac6f5bd6a9e47
thebase <73353819+thebase666@users.noreply.github.com>
2023-01-02 12:24:29 +0700

    c
    c

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,18 +1,26 @@
 import faker from "faker";
 import { useEffect, useState } from "react";
+import logo from '../images.jpg';
 
 function Suggestions() {
     const [suggestions, setsuggestions] = useState([]);//react定义变量的形式
 
-    useEffect(() => {
-        const sugge = [...Array(5)].map((_, i) => ({
-            ...faker.helpers.contextualCard(),
-            id: i,
-        }));
+    useEffect(() => {//for循环
+        let sugge = [];
+        for (var i = 0; i < 6; i++) {
+            let sug = {};
+            sug = { ...faker.helpers.contextualCard(), id: i };
+            sugge.push(sug);
+        }
+        // const sugge = [...Array(5)].map((_, i) => ({
+        //     ...faker.helpers.contextualCard(),
+        //     id: i,
+        // }));
         setsuggestions(sugge);
-    // console.log([...Array(20)]);
-    // console.log(suggestions);
+        // console.log([...Array(20)]);
+        // console.log(suggestions);
     }, []);
+
     return (
         <div className="mt-4 ml-10">
             <div className="flex justify-between mb-5 text-sm">
@@ -21,20 +29,19 @@ function Suggestions() {
             </div>
 
             {suggestions.map((profile) => (
-                <div key={profile.id} className="flex items-center justify-between mt-3"> 
-                    <img className="w-10 h-10 rounded-full border p-[2px]" src="https://i3.nichenggu.com/img/0027/g1nbdqt1fbp.jpg" alt="" />
+                <div key={profile.id} className="flex items-center justify-between mt-3">
+                    <img className="w-10 h-10 rounded-full border p-[2px]"
+                        src={logo.src} alt="" />
                     <div className="flex-1 ml-4 ">
                         <h2 className="text-xs font-semibold">{profile.username}</h2>
                         <h3 className="text-xs text-gray-400">Works at {profile.company.name}</h3>
                     </div>
                     <button className="text-xs font-bold text-blue-400">Follow</button>
-                </div> 
+                </div>
             ))}
 
-
-
         </div>
-        )
-    }
+    )
+}
 
 export default Suggestions
